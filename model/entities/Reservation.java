be91715a515d98010e5c2cfefbb7ddf5525557af
biangomes <biangomes0707@gmail.com>
2022-08-19 17:42:45 -0500

    (#1) debug feito e viu que esta sendo pego a data de janeiro

--- model/entities/Reservation.java
+++ model/entities/Reservation.java
@@ -40,6 +40,11 @@ public class Reservation {
 
     public String updateDates(Date checkIn, Date checkOut) {
 
+        /*
+         * ao debuggar, vi que a data devolvida está em JANEIRO
+         * checkIn: "Sat Jan 22 00:08:00 COT 2022"
+         * checkOut: "Mon Jan 24 00:08:00 COT 2022"
+         */
         Date now = new Date();
 
         // se a data de checkIn for antes de agora OU checkOut for antes de AGORA
